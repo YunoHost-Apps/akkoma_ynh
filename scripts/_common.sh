67a133a705a41b13b7720ac67a4bb2be5396b5c3
lapineige <lapineige@users.noreply.github.com>
2025-01-03 11:16:44 +0100

    export Flavour

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,7 @@
 
 # dependencies used by the app (must be on a single line)
 #REMOVEME? pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg exiftool libimage-exiftool-perl"
+export FLAVOUR="amd64-debian-bookworm"
 
 #=================================================
 # PERSONAL HELPERS
